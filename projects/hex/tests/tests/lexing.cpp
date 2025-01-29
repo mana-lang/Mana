8896957ca7b1d4b98e65d035f97d940cbb6c8686
Nyxa <arnyxxa@gmail.com>
2025-01-29 19:41:08 +0100

    update cmakelists to get ctest detecting hex tests

--- projects/hex/tests/tests/lexing.cpp
+++ projects/hex/tests/tests/lexing.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Lexer", "[lex][token][operator][keyword]") {
+TEST_CASE("Hex Lexer (Hexer)", "[lex][token][operator][keyword]") {
     SECTION("MFT", "Minimal functionality test") {
         REQUIRE(true);
     }
