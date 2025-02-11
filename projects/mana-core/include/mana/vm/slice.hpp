8d0ad8fa4f71ce66ac69920eab1fd3d17bd9105b
Nyxa <arnyxxa@gmail.com>
2025-02-11 04:07:17 +0100

    move slice to its own cpp file in mana-core

--- projects/mana-core/include/mana/vm/slice.hpp
+++ projects/mana-core/include/mana/vm/slice.hpp
@@ -16,36 +16,14 @@ class Slice {
     std::vector<Value> constants;
 
 public:
-    void Write(Op opcode) {
-        code.push_back(static_cast<u8>(opcode));
-    }
+    void Write(Op opcode);
+    void Write(Op opcode, u8 byte);
+    usize AddConstant(Value value);
 
-    void Write(Op opcode, const u8 byte) {
-        code.push_back(static_cast<u8>(opcode));
-        code.push_back(byte);
-    }
-
-    usize AddConstant(const Value value) {
-        constants.push_back(value);
-
-        return constants.size() - 1;
-    }
-
-    auto Code() const -> const std::vector<u8>& {
-        return code;
-    }
-
-    auto Code() -> std::vector<u8>& {
-        return code;
-    }
-
-    auto Constants() const -> const std::vector<Value>& {
-        return constants;
-    }
-
-    MANA_NODISCARD Value ConstantAt(const i64 index) const {
-        return constants[index];
-    }
+    MANA_NODISCARD auto Code() const -> const std::vector<u8>&;
+    MANA_NODISCARD auto Code() -> std::vector<u8>&;
+    MANA_NODISCARD auto Constants() const -> const std::vector<Value>&;
+    MANA_NODISCARD Value ConstantAt(i64 index) const;
 
     // serializes a slice to a vector of unsigned char (bytes)
     // for now, the sequence is:
@@ -53,57 +31,10 @@ public:
     // - constant pool
     // - opcode size
     // - opcode
-    MANA_NODISCARD auto Serialize() const -> std::vector<u8> {
-        constexpr auto size_elem = sizeof(u64);
-
-        const auto constants_count = constants.size();
-        const auto constants_bytes = sizeof(Value) * constants_count;
-
-        const auto code_count = code.size();
-        const auto code_bytes = sizeof(u8) * code_count;
-
-        // ----------------- we also need store 2 slots for array sizes
-        std::vector<u8> ret((size_elem * 2) + constants_bytes + code_bytes);
-
-        std::memcpy(ret.data(), &constants_count, size_elem);
-        std::memcpy(ret.data() + size_elem, constants.data(), constants_bytes);
-
-        const auto code_offset = size_elem + constants_bytes;
-        std::memcpy(ret.data() + code_offset, &code_count, size_elem);
-        std::memcpy(ret.data() + code_offset + size_elem, code.data(), code_bytes);
-
-        return ret;
-    }
+    MANA_NODISCARD auto Serialize() const -> std::vector<u8>;
 
     // for now, this function assumes the input is actually correct
-    bool Deserialize(const std::vector<u8>& bytes) {
-        if (bytes.empty()) {
-            // LogErr("Empty vector given to Slice::Deserialize");
-            return false;
-        }
-
-        constants.clear();
-        code.clear();
-
-        constexpr auto size_elem = sizeof(u64);
-
-        u64 total_constants = 0;
-        std::memcpy(&total_constants, bytes.data(), size_elem);
-
-        const u64 constants_bytes = sizeof(Value) * total_constants;
-
-        constants.resize(total_constants);
-        std::memcpy(constants.data(), bytes.data() + size_elem, constants_bytes);
-
-        u64       total_code  = 0;
-        const u64 code_offset = constants_bytes + size_elem;
-        std::memcpy(&total_code, bytes.data() + code_offset, size_elem);
-
-        code.resize(total_code);
-        std::memcpy(code.data(), bytes.data() + code_offset + size_elem, total_code);
-
-        return true;
-    }
+    bool Deserialize(const std::vector<u8>& bytes);
 };
 
 }  // namespace mana::vm
