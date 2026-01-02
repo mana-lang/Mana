be7d6452c5ac5157d7cd5b36c48f2ce55e66bb32
Nyxa <arnyxxa@gmail.com>
2026-01-02 20:27:21 +0100

    z

--- projects/mana-core/tests/tests/values.cpp
+++ projects/mana-core/tests/tests/values.cpp
@@ -1,6 +1,6 @@
-#include <catch2/catch_test_macros.hpp>
 #include <mana/vm/value.hpp>
 
+#include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 using namespace mana;
