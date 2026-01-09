f6974914471624c839dd51e02371bdaf9df39c33
Nyxa <arnyxxa@gmail.com>
2026-01-09 23:15:24 +0100

    refactor Hex to handle register-based ops

--- projects/hex/include/hex/vm/virtual-machine.hpp
+++ projects/hex/include/hex/vm/virtual-machine.hpp
@@ -16,29 +16,12 @@ enum class InterpretResult {
 class VirtualMachine {
     ml::u8* ip {nullptr};
 
-    std::vector<mvm::Value> locals;
-    std::vector<mvm::Value> stack;
-    mvm::Value*             stack_top;
+    std::vector<mvm::Value> registers;
 
 public:
     VirtualMachine();
 
     InterpretResult Interpret(mvm::Slice* next_slice);
-
-    HEX_NODISCARD ml::u64 StackSize() const;
-
-private:
-    void Reset();
-
-    void       Push(const mvm::Value& value);
-    mvm::Value Pop();
-
-    HEX_NODISCARD mvm::Value ViewTop() const;
-    HEX_NODISCARD mvm::Value* StackTop() const;
-
-    void LogTop(std::string_view msg) const;
-    void LogTopTwo(std::string_view msg) const;
-    void LogLocalDatum(std::string_view msg) const;
 };
 
 }  // namespace hex
