0f0ee75d334b64824bec06278d6ab7a0a9c55d77
Nyxa <arnyxxa@gmail.com>
2026-01-11 03:27:58 +0100

    add new cli functionality to circe

---
+++ projects/circe/src/circe/core/cli.cpp
@@ -0,0 +1,36 @@
+#include <circe/core/cli.hpp>
+
+namespace circe {
+CommandLineSettings::CommandLineSettings(int argc, char** argv)
+    : argc{argc}
+    , argv{argv}
+    , verbose {false}
+    , emit_ptree {false}
+    , emit_tokens {false} {
+    cli = std::make_unique<CLI::App>("Circe, the Mana Compiler");
+}
+
+int CommandLineSettings::Populate() {
+    cli->add_option("input", input, "The Mana file to compile.")->required();
+
+    cli->add_option("-o,--output", output, "The output executable name.");
+
+    cli->add_flag("-v,--verbose", verbose, "Verbose output.");
+    cli->add_flag("-p,--ptree", emit_ptree, "Emit AST after compilation.");
+    cli->add_flag("-t,--tokens", emit_tokens, "Emit tokens after lexing.");
+
+    try {
+        cli->parse(argc, argv);
+    }
+    catch (const CLI::ParseError& e) {
+        return cli->exit(e);
+    }
+    return 0;
+}
+
+std::string CommandLineSettings::InputFile() const { return input; }
+std::string CommandLineSettings::OutputFile() const { return output; }
+bool CommandLineSettings::EmitVerbose() const { return verbose; }
+bool CommandLineSettings::EmitParseTree() const { return emit_ptree; }
+bool CommandLineSettings::EmitTokens() const { return emit_tokens;}
+}
--- projects/circe/src/circe/main.cpp
+++ projects/circe/src/circe/main.cpp
@@ -1,5 +1,6 @@
 #include <circe/core/logger.hpp>
 #include <circe/main-visitor.hpp>
+#include <circe/core/cli.hpp>
 
 #include <sigil/ast/lexer.hpp>
 #include <sigil/ast/parser.hpp>
@@ -10,58 +11,126 @@
 #include <filesystem>
 #include <fstream>
 
+#include <mana/exit-codes.hpp>
+
 using namespace mana::literals;
 using namespace circe;
 
 constexpr std::string IN_PATH  = "assets/samples/";
 constexpr std::string OUT_PATH = "../hex/";
 
-int CompileFrom(const std::string& filename) {
-    Log->info("");
+int CompileFrom(const std::filesystem::path& in_path,
+                std::filesystem::path out_path,
+                bool verbose,
+                bool emit_ptree,
+                bool emit_tokens
+) {
+    namespace chrono = std::chrono;
+    using namespace mana;
 
+    const auto time_total_start = chrono::high_resolution_clock::now();
+
+    const auto time_lex_start = chrono::high_resolution_clock::now();
     sigil::Lexer lexer;
-
-    const std::filesystem::path in = IN_PATH + filename;
-
-    if (lexer.Tokenize(in)) {
-        Log->info("Tokenized file from '{}'", in.string());
+    if (not lexer.Tokenize(in_path)) {
+        return Exit(ExitCode::LexerError);
     }
+    const auto time_lex_end = chrono::high_resolution_clock::now();
 
-    sigil::Parser parser(lexer.RelinquishTokens());
-    if (parser.Parse()) {
-        Log->info("Parsed the file.");
+    auto tokens            = lexer.RelinquishTokens();
+    const auto token_count = tokens.size();
+
+    const auto time_parse_start = chrono::high_resolution_clock::now();
+    sigil::Parser parser(std::move(tokens));
+    if (not parser.Parse()) {
+        return Exit(ExitCode::ParserError);
     }
+    const auto time_parse_end = chrono::high_resolution_clock::now();
 
-    const auto& ast = parser.ViewAST();
-    Log->info("Compiling...");
-    parser.PrintParseTree();
-
+    const auto time_codegen_start = chrono::high_resolution_clock::now();
+    const auto& ast               = parser.ViewAST();
     CirceVisitor visitor;
     ast->Accept(visitor);
 
-    mana::vm::Slice slice;
-    slice = visitor.GetSlice();
+    auto slice                  = visitor.GetSlice();
+    const auto time_codegen_end = chrono::high_resolution_clock::now();
 
-    std::filesystem::path out = OUT_PATH + in.filename().replace_extension("hexe").string();
-    std::ofstream         out_file(out, std::ios::binary);
-    Log->info("Output file to '{}'", out.string());
+    // If no output path is provided, use the input filename with .hexe extension
+    if (out_path.empty()) {
+        out_path = in_path;
+        out_path.replace_extension("hexe");
+    }
+
+    const auto time_write_start = chrono::high_resolution_clock::now();
+    std::ofstream out_file(out_path, std::ios::binary);
+    if (not out_file) {
+        return Exit(ExitCode::OutputOpenError);
+    }
 
     const auto output = slice.Serialize();
     out_file.write(reinterpret_cast<const char*>(output.data()), static_cast<std::streamsize>(output.size()));
 
-    int ret = 0;
     if (not out_file) {
-        Log->error("Failed to write to file.");
-        ret = 3;
+        return Exit(ExitCode::OutputWriteError);
     }
 
-    out_file.close();
-    return ret;
+    // write ends when total ends
+    const auto time_total_end = chrono::high_resolution_clock::now();
+
+    const auto time_lex     = chrono::duration_cast<chrono::microseconds>(time_lex_end - time_lex_start);
+    const auto time_parse   = chrono::duration_cast<chrono::microseconds>(time_parse_end - time_parse_start);
+    const auto time_codegen = chrono::duration_cast<chrono::microseconds>(time_codegen_end - time_codegen_start);
+    const auto time_write   = chrono::duration_cast<chrono::microseconds>(time_total_end - time_write_start);
+    const auto time_total   = chrono::duration_cast<chrono::microseconds>(time_total_end - time_total_start);
+
+    const auto compile_str = fmt::format("Compiled '{}' => '{}'",
+                                         in_path.filename().string(),
+                                         out_path.filename().string()
+    );
+
+    Log->info(compile_str);
+    Log->info("Operation completed in {}us", time_total.count());
+
+    const auto divider = std::string(compile_str.size(), '-');
+
+    if (verbose) {
+        Log->info(divider);
+        Log->info("  Tokens:         {}", token_count);
+        Log->info("  Instructions:   {} bytes", slice.Instructions().size());
+        Log->info("  Constant Pool:  {} constants ({} bytes)", slice.ConstantCount(), slice.ConstantPoolBytesCount());
+        Log->info("  Executable:     {} bytes", output.size());
+        Log->info("");
+        Log->info("  == Lex:     {}us", time_lex.count());
+        Log->info("  == Parse:   {}us", time_parse.count());
+        Log->info("  == Codegen: {}us", time_codegen.count());
+        Log->info("  == Write:   {}us", time_write.count());
+        Log->info("");
+        Log->info("  ---- Total: {}us", time_total.count());
+    }
+
+    if (emit_ptree) {
+        Log->info("{}\n", divider);
+        parser.PrintParseTree();
+    }
+
+    if (emit_tokens) {
+        Log->info("{}\n", divider);
+        sigil::PrintTokens(parser.ViewTokenStream());
+    }
+
+    return Exit(ExitCode::Success);
 }
 
-int main() {
-    using namespace circe;
-    Log->info("Hello from Circe!");
+int main(int argc, char** argv) {
+    CommandLineSettings cli(argc, argv);
+    if (int result = cli.Populate();
+        result != 0) {
+        return result;
+    }
 
-    CompileFrom("6-data.mn");
+    if (cli.InputFile().empty()) {
+        return mana::Exit(mana::ExitCode::NoFileProvided);
+    }
+
+    return CompileFrom(cli.InputFile(), cli.OutputFile(), cli.EmitVerbose(), cli.EmitParseTree(), cli.EmitTokens());
 }
